5c23c7fe2bd722c133f4b152ba8b1c82d2936299
Potato <Potato@PotatoIsCool.com>
2026-06-21 12:43:09 -0500

    Added Dependencies to the project.

---
+++ LunaRR/src/Dependencies/Luma/Readme.cpp
@@ -0,0 +1,11 @@
+/*
+ALL OWNERSHIP GOES TO @Pillowguy._. & PotatoIsCool
+If you paid for this src you have been scammed.
+https://github.com/PotatoIsCoolio
+https://github.com/pillowman123
+*/
+
+
+// If you are reading this well then you are probably wondering where is the il2cpp resolver. 
+//For personal reason we will be keeping this privated as we do not feel that the public should have it.
+// If you are trying to rebuild this project you will need to completely remake your own il2cpp resolver OR import your own and change all the classes and namespaces to your own.
